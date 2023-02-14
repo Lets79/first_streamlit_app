2bc4326621fd3144e5ba35a7ed4a6fd2d9e3815c
Lets79 <124713821+Lets79@users.noreply.github.com>
2023-02-14 14:59:22 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,10 @@ streamlit.header("Fruityvice Fruit Advice!")
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
 
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.write('The user entered ', fruit_choice)
+streamlit.write('The user entered ', fruit_choice)
 
 
 
