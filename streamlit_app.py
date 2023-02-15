ce378602d323deb6d2c2e1c60e64148d4d8ba807
Lets79 <124713821+Lets79@users.noreply.github.com>
2023-02-15 11:44:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,16 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show) 
 
+
+#create repeatable code block (called a funtion)
+def get fruityvice_data(this_fruit_choice):
+        #streamlit.write('The user entered ', fruit_choice)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+        #Take the json version of the response and normalize it
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        return fruityvice_normalized
+
+#new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
 
@@ -34,12 +44,9 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        #streamlit.write('The user entered ', fruit_choice)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        #Take the json version of the response and normalize it
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        back_from_funtion = get_fruityvice_data(fruit_choice)
         # output it on the screen as a table
-        streamlit.dataframe(fruityvice_normalized)
+        streamlit.dataframe(back_from_function)
     
 except Urlerror as e:
     streamlist.error()
